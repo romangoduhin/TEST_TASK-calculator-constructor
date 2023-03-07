014bff1e2e897bc3b4b3423d0bb69b789baad790
Roman Goduhin <superchewits2000@gmail.com>
2023-03-07 13:17:06 +0300

    feat: implement Switch component

--- my-app/src/components/Constructor/Switch/Switch.tsx
+++ my-app/src/components/Constructor/Switch/Switch.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import eyeIconSvg from '../../../assets/eyeIcon.svg';
+import constructorIconSvg from '../../../assets/constructorIcon.svg';
 
 function Switch() {
+  const [isRuntime, setIsRuntime] = useState(false);
+
   return (
-    <div>Switch</div>
+    <div className=" w-[243px] h-[38px] custom-switch ">
+      <button
+        type="button"
+        onClick={() => setIsRuntime(true)}
+        className={`w-[108px] ${isRuntime ? 'active-switch-btn switch-btn' : 'switch-btn'}`}
+      >
+        <img src={eyeIconSvg} alt="runtime-icon" />
+        <span>Runtime</span>
+      </button>
+
+      <button
+        type="button"
+        onClick={() => setIsRuntime(false)}
+        className={`w-[133px] ${!isRuntime ? 'active-switch-btn switch-btn' : 'switch-btn'}`}
+      >
+        <img src={constructorIconSvg} alt="constructor-icon" />
+        <span>Constructor</span>
+      </button>
+    </div>
   );
 }
 
