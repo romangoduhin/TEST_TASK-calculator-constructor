fdac1b52f240787b97634b70e085981d57a568cc
Roman Goduhin <superchewits2000@gmail.com>
2023-03-07 12:25:33 +0300

    feat: implement Constructor and Canvas templates

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Palette from './components/Palette/Palette';
+import Constructor from './components/Constructor/Constructor';
 
 function App() {
   return (
     <div className="w-screen h-screen flex-centred bg-orange-200">
-      <div className="w-[695px] h-[640px] bg-red-200">
+      <div className="w-[695px] h-[640px] flex flex-row bg-red-200">
         <Palette />
+        <Constructor />
       </div>
     </div>
   );
